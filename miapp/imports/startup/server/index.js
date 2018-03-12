1de8c1995dd7dc5b0a1268c5da6800b184268b3c
Arturo Maltos <m4ltos@gmail.com>
2018-03-12 15:10:53 -0400

    Ch1 Preparing the project

---
+++ miapp/imports/startup/server/index.js
@@ -0,0 +1,5 @@
+// entry point from the automatically compiled inside the server
+import { createApolloServer } from 'meteor/apollo';
+import { makeExecutableSchema } from 'graphql-tools';
+
+createApolloServer({ });
